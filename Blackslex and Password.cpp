6d555f05d97daef0bb0b48f05894bbefe4f3782f
Sohag Hossain <sohag3769@gmail.com>
2026-04-23 17:47:59 +0600

    Add files via upload

---
+++ Blackslex and Password.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+// int need_thing(vector<int> &a ){
+    
+//     unordered_set<int> s(a.begin(), a.end());
+    
+    
+//     int mx = 0;
+    
+//     while(s.count(mx)) mx++;
+    
+
+//     return mx;
+    
+    
+// }
+
+
+
+#define ll long long
+
+
+
+
+int main(){
+    
+    
+     int t;
+    cin>>t;
+    while(t--){
+        
+        int k , x;
+        
+        cin>>k>>x;
+        
+        
+        int n = k * x + 1;
+        
+        cout << n << endl;
+    }
+    
+    
+    
+    return 0;
+}
